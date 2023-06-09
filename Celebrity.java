a0d51ecf758373f228fdc7d2ccec189e40b43aea
BigTimeAcorns <cadejdannels@gmail.com>
2023-06-09 16:03:36 -0700

    eee

---
+++ Celebrity.java
@@ -0,0 +1,25 @@
+package CelbrityLab;
+
+public class Celebrity {
+    private String name;
+    private String guess;
+    private String type;
+
+    public Celebrity(String n, String g, String t) {
+        this.name = n;
+        this.guess = g;
+        this.type = t;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGuess() {
+        return guess;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
